61fa2a21e4442587908af0ea8e1592995415569f
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-21 10:41:29 +0300

    Why is git like this

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 app.get('/api/parking/:id', async (req, res) => {
   const parkingId = req.params.id;
-  const parkings = await getParkings();
+  const parkings = await getParkings(); 
   requestedParking = parkings.find((parking) => parking.id === parkingId);
 
   if (!requestedParking) {
